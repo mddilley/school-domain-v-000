47dc691b6d3b0472632e300d7625376234f1c54e
Mike Dilley <mike.dilley@gmail.com>
2018-10-15 21:34:36 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -15,9 +15,12 @@ class School
   end
 
   def add_student(name, grade)
-
-    @roster[grade] = []
-    @roster[grade] << name
+    if @roster[grade] != nil
+      @roster[grade] = []
+      @roster[grade] << name
+    else
+      @roster[grade] << name
+    end
   end
 
 
