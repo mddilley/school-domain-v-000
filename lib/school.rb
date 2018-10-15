629fca71e5f01772dbce8920e7a0a8080c6a11b6
Mike Dilley <mike.dilley@gmail.com>
2018-10-15 21:11:09 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,14 +2,19 @@
 
 class School
 
-  attr_accessor :roster
+
 
   def initialize(roster)
     @roster = roster
   end
 
+  def roster
+    @roster = {}
+  end
 
-
+  def add_student(name, grade)
+    @roster[grade] = name
+  end
 
 
 end
